746c21ff38f4ff26da8b68c55e8a70f70a503e6d
arikislam <arikislam321@gmail.com>
2016-12-10 01:35:28 +0600

    Day 1 Progress

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
 
 
 Route::get('articels','ArticelController@index');
+Route::get('articels/create','ArticelController@create');
+
 
 Route::get('articels/{id}','ArticelController@show');
+Route::post('articels','ArticelController@store');
+
 
